f4f2d5f915de712756d8a0ec881784a877412905
Beverly Pham <beverly.pham@gmail.com>
2018-04-15 20:24:20 -0700

    more initial setup

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = [
     'cryptacular',
     'plaster_pastedeploy',
+    'psycopg2',
     'pyramid >= 1.9a',
     'pyramid_debugtoolbar',
     'pyramid_jinja2',
     'pyramid_retry',
     'pyramid_tm',
-    'psycopg2',
     'requests',
     'SQLAlchemy',
     'transaction',
