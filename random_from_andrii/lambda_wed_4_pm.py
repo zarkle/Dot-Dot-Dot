0e71583aa4331100f4038e1e71301405ba9f812e
Beverly Pham <beverly.pham@gmail.com>
2018-04-19 09:49:56 -0700

    cleaning up code

--- random_from_andrii/lambda_wed_4_pm.py
+++ random_from_andrii/lambda_wed_4_pm.py
@@ -1,18 +1,15 @@
-from __future__ import print_function
 import os
 from urllib.request import urlopen
 import urllib
-from urllib.parse import urlencode as urlencode
-import xml.etree.ElementTree as ET
-import re, string
+import re
+import string
 import json
 from random import randint
 
 
 def lambda_handler(event, context):
     """
-    this function is a route handler that routes incoming launchRequest or intentRequsts
-    referenced codebase from: https://github.com/n8henrie
+    this function is a route handler that routes incoming LaunchRequest or IntentRequest
     """
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
@@ -35,8 +32,8 @@ def on_session_started(session_started_request, session):
 
 
 def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
+    """
+    Called when the user launches the skill without specifying what they want
     """
     print("on_launch requestId=" + launch_request['requestId'] +
           ", sessionId=" + session['sessionId'])
@@ -50,18 +47,14 @@ def on_intent(intent_request, session):
           ", sessionId=" + session['sessionId'])
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
-    print('intent', intent)
-    print('intent name', intent_name)
-    # Dispatch to your skill's intent handlers
-    if intent_name == "wolfman":
-        return get_WolfRam(intent, session)
-    else:
-        speech_output = "Can You repeat your question?"
-        return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, should_end_session))
 
+    # Dispatch to your skill's intent handlers
+    if intent_name == 'wolfman':
+        return get_WolfRam(intent, session)
 
 def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
+    """
+    Called when the user ends the session.
     Is not called when the skill returns should_end_session=true
     """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
@@ -71,44 +64,42 @@ def on_session_ended(session_ended_request, session):
 
 # --------- below controls the skill interaction and behavior ------------
 welcome_message = [
-    "I am wolfman, Alexa's nerdy alter ego.  To ask me a question, phrase your question like this. wolfman, why are firetrucks red.",
-    "Don't tell Alexa, but I am way smarter than she is.  To ask me a question, phrase your question like this. wolfman, how far away is the moon in inches.",
-    "I am wolfman, Ask me a question like this. wolfman, why is the sky blue. or, wolfman, how far away is saturn.",
-    "You can ask me almost any question, just make sure to start them with my name. wolfman"
+    'I am wolfman, Alexa\'s nerdy alter ego.  To ask me a question, phrase your question like this. wolfman, why are firetrucks red.',
+    'Don\'t tell Alexa, but I am way smarter than she is.  To ask me a question, phrase your question like this. wolfman, how far away is the moon in inches.',
+    'I am wolfman, Ask me a question like this. wolfman, why is the sky blue. or, wolfman, how far away is saturn.',
+    'You can ask me almost any question, just make sure to start them with my name. wolfman'
     ]
 
 
 def get_welcome_response():
     """conrols welcome message on skill open"""
     session_attributes = {}
-    card_title = "Welcome to Wolfram"
-    random = randint(1,4)
+    card_title = 'Welcome to Wolfman'
+    random = randint(1, 5) - 1
     speech_output = welcome_message[random]
-    reprompt_text = "Can you repeat the question"
+    reprompt_text = 'Can you repeat the question?'
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
 def get_WolfRam(intent, session):
-    """controls get function to wolfram api's"""
+    """controls get function to wolfram's APIs"""
     try:
         session_attributes = {}
         should_end_session = False
-        reprompt_text = "I didn't catch that. Please phrase your question like this. wolfman, what is the meaning of life?"
-        speech_output = "Try asking a question you would ask Wolfram Alpha."
-        appid = os.environ["WOLFRAM_ID"]
+        reprompt_text = 'I didn\'t catch that. Please phrase your question like this. wolfman, what is the meaning of life?'
+        speech_output = 'Try asking a question you would ask Wolfram Alpha.'
+        appid = os.environ['WOLFRAM_ID']
 
         query = intent['slots']['response'].get('value')
         if not query:
             speech_output = "Can You repeat your question?"
             return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, should_end_session))
         query = re.sub('[%s]' % re.escape(string.punctuation), '', query).replace(' ', '+')
-        url = "http://api.wolframalpha.com/v1/spoken?i="+query + "&appid=" + appid
-        url1 = "http://api.wolframalpha.com/v1/result?i="+query + "%3F&appid=" + appid
-        print('url - ', url)
-        print('url2 - ', url1)
-        if url == None:
+        url = 'http://api.wolframalpha.com/v1/spoken?i=' + query + '&appid=' + appid
+        url1 = 'http://api.wolframalpha.com/v1/result?i=' + query + '&appid=' + appid
+        if url is None:
             pass
         try:
             data = urlopen(url)
@@ -117,50 +108,48 @@ def get_WolfRam(intent, session):
                 data = urlopen(url1)
             except urllib.error.URLError as e:
                 try:
-                    url2 = "https://api.wolframalpha.com/v2/query?input="+query + "&format=plaintext&output=JSON&appid=" + appid
-                    print(url2)
+                    url2 = 'https://api.wolframalpha.com/v2/query?input=' + query + '&format=plaintext&output=JSON&appid=' + appid
                     data = urlopen(url2)
                     tree = data.read().decode('utf-8')
                     tree = json.loads(tree)
                     try:
                         tree = tree['queryresult']['pods'][1]['subpods'][0]['plaintext']
                         tree = re.sub('[%s]' % re.escape(string.punctuation), '', tree)
-                        speech_output = "Wolfram Alpha says " + tree
+                        speech_output = 'Wolfram Alpha says ' + tree
                         return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, should_end_session))
                     except e:
-                        speech_output = "Can You repeat your question?"
+                        speech_output = 'Can You repeat your question?'
                         return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, should_end_session))
                 except urllib.error.URLError as e:
-                    speech_output = "I may be smarter than alexa, but even I have my limits.  Try asking in a different way, and make sure you start your question with by saying my name, wolfman."
-                    print(speech_output)
+                    speech_output = 'I may be smarter than alexa, but even I have my limits.  Try asking in a different way, and make sure you start your question with by saying my name, wolfman.'
                     return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, should_end_session))
 
         tree = data.read().decode('utf-8')
         tree = re.sub('[%s]' % re.escape(string.punctuation), '', tree)
 
-        """generate a random output response prefix"""
-
+        # generate a random output response prefix
         custom_anwer = [
-            "Ziggy says " + str(tree),
-            "That was easy to find, " + str(tree),
-            "Had to dust off the old encyclopedia on that one. " + str(tree),
-            str(tree) + ", Boom!",
-            "Hal says " + str(tree),
-            "Hmmmm. let me think. " + str(tree),
-            "I don't know the answer to that... just kidding, " + str(tree),
-            "My magic eight ball says " + str(tree),
-            "Beep bop boop boop beep boop bop beep. " + str(tree),
-            "Let me google that for you. " + str(tree)
+            'Ziggy says ' + str(tree),
+            'That was easy to find, ' + str(tree),
+            'Had to dust off the old encyclopedia on that one. ' + str(tree),
+            str(tree) + ', Boom!',
+            'Hal says ' + str(tree),
+            'Hmmmm. let me think. ' + str(tree),
+            'I don\'t know the answer to that... just kidding, ' + str(tree),
+            'My magic eight ball says ' + str(tree),
+            'Beep bop boop boop beep boop bop beep. ' + str(tree),
+            'Let me google that for you. ' + str(tree)
             ]
 
-        random = randint(1,10)
+        random = randint(1, 10)
         speech_output = custom_anwer[random]
 
         return build_response(session_attributes, build_speechlet_response(
             intent['name'], speech_output, reprompt_text, should_end_session))
     except:
-        speech_output = "Hmmm, ask me again and make sure you start with my name, wolfman. or you could just google it."
+        speech_output = 'Hmmm, ask me again and make sure you start with my name, wolfman.'
         return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, should_end_session))
+
 
 def multiple_replace(dict, text):
     """
