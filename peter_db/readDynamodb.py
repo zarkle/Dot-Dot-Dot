8bd81e04a9562dfe3b8ba70c89d71c05d93e445a
Peter Kim <seattlechem@yahoo.com>
2018-04-19 11:13:34 -0700

    peter_db folder deleted

--- peter_db/readDynamodb.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    import boto3
-
-    client = boto3.resource('dynamodb', region_name='us-east-1')
-    table = client.Table('wolf_db')
-
-    result = table.scan()
-    return(result)
